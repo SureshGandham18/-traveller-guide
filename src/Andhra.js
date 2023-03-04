8e35da5b0e7445c35e7faefffe57ab9c493c8fc0
SureshGandham18 <85335126+SureshGandham18@users.noreply.github.com>
2023-03-04 16:52:46 +0530

    1 file modified

--- src/Andhra.js
+++ src/Andhra.js
@@ -5,103 +5,127 @@ export default function Andhra() {
     <>
       <center>
         <Title>
-          <i>
-            <img src="" />
-            ANDHRA PRADESH
-          </i>
+          <img src="" />
+          ANDHRA PRADESH
         </Title>
       </center>
       <Cont>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          &nbsp; "The Heritage Haven of Andhra" Anantapur Tourism Anantapuram is
-          the headquarters of Anantapur District in the Indian state of Andhra
-          Pradesh. Anantapuram is not a cosmopolitan city. Neither is it a
-          village. It is one of those towns in transition, that is at the
-          confluence of the past and the future. Anantapuram reverberates with
-          flashes of India's glorious history and the true ethnic traditions and
-          values of India. Interestingly, Anantapur receives the second-lowest
-          rainfall in India. Anantapur has seen the rule of many empires, but
-          the Vijayanagar Empire has had a greater impact on it. It is said that
-          the place has derived its name from 'Anaatasagaram', a big tank, which
-          translates into 'Endless Ocean'. Chikkavodeya, the minister of the
-          Vijayanagar King, Bukka-I constructed the town of Anaatasagaram and
-          Bukkarayasamudram.
+          <img src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg " />
+          <h2>"The Heritage Haven of Andhra"</h2> <h3>Anantapur Tourism</h3>{' '}
+          Anantapuram is the headquarters of Anantapur District in the Indian
+          state of Andhra Pradesh. Anantapuram is not a cosmopolitan city.
+          Neither is it a village. It is one of those towns in transition, that
+          is at the confluence of the past and the future. Anantapuram
+          reverberates with flashes of India's glorious history and the true
+          ethnic traditions and values of India. Interestingly, Anantapur
+          receives the second-lowest rainfall in India. Anantapur has seen the
+          rule of many empires, but the Vijayanagar Empire has had a greater
+          impact on it. It is said that the place has derived its name from
+          'Anaatasagaram', a big tank, which translates into 'Endless Ocean'.
+          Chikkavodeya, the minister of the Vijayanagar King, Bukka-I
+          constructed the town of Anaatasagaram and Bukkarayasamudram.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/bgImages/ARAKU-VALLEY.jpg"
-            height="130px"
-          />
-          "Hill Station of Andhra Pradesh" Araku Valley Tourism Araku Valley is
-          a comparatively unexplored hill station mostly visited by locals as a
-          weekend getaway and is situated some 120 kilometres away from
-          Vishakhapatnam. If you're a tourist looking for exclusivity and
-          tranquillity, Vistadome train leaves Vishakhapatnam at 6.50 AM, making
-          way through 58 tunnels and passing over 84 bridges making way through
-          the breathtaking landscape to reach Araku in approximately in 5 hours,
-          go, catch that!
+          <img src="https://www.holidify.com/images/bgImages/ARAKU-VALLEY.jpg" />
+          <h2>"Hill Station of Andhra Pradesh"</h2>{' '}
+          <h3>Araku Valley Tourism</h3> Araku Valley is a comparatively
+          unexplored hill station mostly visited by locals as a weekend getaway
+          and is situated some 120 kilometres away from Vishakhapatnam. If
+          you're a tourist looking for exclusivity and tranquillity, Vistadome
+          train leaves Vishakhapatnam at 6.50 AM, making way through 58 tunnels
+          and passing over 84 bridges making way through the breathtaking
+          landscape to reach Araku in approximately in 5 hours, go, catch that!
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/bgImages/VISAKHAPATNAM.jpg" />
+          <h2>"A gem of South India"</h2> <h3>Visakhapatnam Tourism</h3>{' '}
+          Visakhapatnam, also commonly known as Vizag, is one of the oldest port
+          cities in the country. Situated in the heart of Andhra Pradesh, Vizag
+          was announced as the state capital of Andhra Pradesh on January 31,
+          2023. The city is known for its picturesque beaches and serene
+          landscape, as well as a rich cultural past. The port of Visakhapatnam
+          is renowned for being home to the oldest shipyard in all of India.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/bgImages/AMARAVATHI.jpg" />
+          <h2>"Capital City of Andhra Pradesh"</h2>
+          <h3>Amaravathi Tourism</h3>Amaravathi Known as the Capital of Andhra
+          Pradesh, Amaravathi is a planned city on the banks of the River
+          Krishna. Amaravathi is renowned for being a site of a Buddhist Stupa
+          that is a semi-hemispherical structure containing Buddhist relics and
+          often called the Abode of God.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/bgImages/GANDIKOTA.jpg" />
+          <h2>"Grand Canyon of India"</h2> <h3>Gandikota Tourism</h3>{' '}
+          Endearingly known as the ‘Grand Canyon of India’, Gandikota is a small
+          and quaint village in the Kadapa district of Andhra Pradesh. Flanked
+          by River Pennar on the right side, the village is famous for the
+          spectacular gorges formed by the river cutting through Erramala Hills.
+          The narrow valleys with streams running between them and the steep
+          rocky walls are reminiscent of the renowned Grand Canyon in Arizona.
+          It requires a walk of almost a kilometre from the main gate to reach
+          the boulder from where you can observe the mesmerising view from the
+          viewpoint near the fort.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/compressed/3273.jpg?v=1.1" />
+          <h2>"Andhra's temple town"</h2>
+          <h3>Tirupati Tourism</h3>
+          Situated in the Chittoor district of Andhra Pradesh, Tirupati is known
+          for Lord Venkateshwara Temple, one of the most visited pilgrimage
+          centres in the country. Tirumala is one of the seven hills in
+          Tirupati, where the main temple is located. The temple is believed to
+          be placed where Lord Venkateshwara took the form of an idol and is
+          hence home to the diety Govinda. Tirupati is one of the oldest cities
+          of India and finds mention in plenty of ancient Vedas and Puranas.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/bgImages/SRISAILAM.jpg" />
+          <h2>"The Abode of Jyotirlinga of Andhra Pradesh"</h2>
+          <h3>Srisailam Tourism</h3>
+          Srisailam, a hill town in Kurnool district of Andhra Pradesh is home
+          to the holy Malikarjuna Jyotirlinga and also as one of the eighteen
+          Shakti Peethas of goddess Parvati. Situated on the banks of river
+          Krishna, Srisailam is identified with a wildlife sanctuary and a dam.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/compressed/3369.jpg?v=1.1" />
+          <h2>"A Trekkers Paradise in Telangana"</h2>
+          <h3>Ananthagiri Hills Tourism</h3>
+          Set amid the Eastern Ghats, Ananthagiri Hills is a beautiful retreat
+          90km away from Hyderabad city. One of the earliest human habitat
+          areas, Ananthagiri is a hill town of ancient caves, temples, medieval
+          fort palaces that showcases the history of the area. The place offers
+          some of the best trekking trails and thus serves as a trekker's
+          paradise. Breathtakingly gorgeous and absolutely scintillating, the
+          town oozes out charm mixed with the whiff of coffee aroma and beauty
+          of exotic flora. With romanticism in every aspect of the region, it is
+          a perfect place for honeymooners as well as family vacationers.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
+          <img src="https://www.holidify.com/images/compressed/696.jpg?v=1.1" />
+          <h2>"The Gateway of Rayalaseema"</h2>
+          <h3>Kurnool Tourism</h3>A place of much historical value with
+          beautiful surrounding areas, Kurnool, is a small town in Andhra
+          Pradesh where much of history left its imprints. The city also is
+          known as The Gateway of Rayalaseema. Kurnool has in its domain remains
+          of the palace of Gopal Raju (the last Hindu king), ruins of a medieval
+          fort built by Vijaynagar Empire having ancient Persian and Arabic
+          inscriptions and the summer palace built by Kurnool rulers during the
+          16th century.
         </Card>
         <Card>
-          <img
-            src="https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1539191288_20200102183329.jpg "
-            height="130px"
-          />
-          bqwediuhjwnedxoimoikemowix,qozkpo,qoweir3dmpofbvisnoiucmoridcgbnsgxefvresg
-          <br />
-          webdinbwie
+          <img src="https://www.holidify.com/images/compressed/696.jpg?v=1.1" />
+          <h2>"A gem of South India"</h2>
+          <h3>Visakhapatnam Tourism</h3>
+          Visakhapatnam, also commonly known as Vizag, is one of the oldest port
+          cities in the country. Situated in the heart of Andhra Pradesh, Vizag
+          was announced as the state capital of Andhra Pradesh on January 31,
+          2023. The city is known for its picturesque beaches and serene
+          landscape, as well as a rich cultural past. The port of Visakhapatnam
+          is renowned for being home to the oldest shipyard in all of India.
         </Card>
       </Cont>
     </>
@@ -117,21 +141,23 @@ const Cont = styled.div`
   padding-right:10rem;
 `;
 const Card = styled.div`
-// border:none;
   background-color:lightgray;
   margin:3rem;
   padding:3rem;
-  // color:white;
   border-radius:5px;
   img{
     float:left;
     padding-right:12px;
+    height:200px;
+    width:400px;
+    border-radius:5px;
   }
 `;
 const Title = styled.div`
 font-size: 80px;
-background: -webkit-linear-gradient(yellow, green);
+background: -webkit-linear-gradient(gray,pink);
 -webkit-background-clip: text;
 -webkit-text-fill-color: transparent;
 font-weight:1000;
+height:100px;
 `;
